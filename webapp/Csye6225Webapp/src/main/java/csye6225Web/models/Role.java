b0474b17acadaa87ec561bae27c605b31f23374a
leo <cheng.li@husky.neu.edu>
2018-10-24 01:31:39 -0400

    commited

--- webapp/Csye6225Webapp/src/main/java/csye6225Web/models/Role.java
+++ webapp/Csye6225Webapp/src/main/java/csye6225Web/models/Role.java
@@ -1,20 +1,20 @@
-package csye6225Web.models;
-
-import javax.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@ToString
-public class Role extends BaseEntity{
-
-    private String name;
-    
-}
+//package csye6225Web.models;
+//
+//import javax.persistence.Entity;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import lombok.ToString;
+//
+//@Entity
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Getter
+//@Setter
+//@ToString
+//public class Role extends BaseEntity{
+//
+//    private String name;
+//
+//}
